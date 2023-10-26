f22c2d8c6b8ef4e1be6c00b84f962f1a11346263
code . <blazej686@yahoo.com>
2023-10-26 14:42:25 -0600

    its working

---
+++ src/components/BlogComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <section class="row">
+        <div class="col-8 border">
+            <p>{{ blogProp.body }}</p>
+        </div>
+        <div class="col-4">
+            <img :src="blogProp.imgUrl" :alt="blogProp.title">
+        </div>
+    </section>
+</template>
+
+
+<script>
+import { AppState } from '../AppState';
+import { computed, reactive, onMounted } from 'vue';
+import { Blog } from '../models/Blog.js';
+export default {
+    props: { blogProp: { type: Blog, required: true } },
+
+
+    setup(props) {
+        return {}
+    }
+};
+</script>
+
+
+<style lang="scss" scoped>
+img {
+    height: 20vh;
+    width: 100%;
+    object-fit: cover;
+    object-position: center;
+}
+</style>
---
+++ src/services/BlogsService.js
@@ -0,0 +1,17 @@
+import { AppState } from "../AppState.js"
+import { Blog } from "../models/Blog.js"
+import { logger } from "../utils/Logger.js"
+import { api } from "./AxiosService.js"
+
+class BlogsService {
+
+    async getBlogs() {
+
+        const res = await api.get('api/blogs')
+        logger.log('blogs', res.data)
+        AppState.blogs = res.data.map((pojo) => new Blog(pojo))
+
+    }
+}
+
+export const blogsService = new BlogsService()
